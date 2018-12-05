7e7b4cb75088f5188f21ce9827e97f34cd96630f
Shintaroh Itakura <shintaroitakura@gmail.com>
2018-12-04 22:02:00 -0800

    slight adjustments looks like it still works

--- project-app/src/components/Form.js
+++ project-app/src/components/Form.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 const Form = (props) => {
-  return (
-      <form onSubmit={props.loadWeather}>
-        <input type="text" name="city" placeholder="Which City ?" />
-        <input type="text" name="country" placeholder="What Country ?" />
-        <button>Search</button>
-      </form>
-  )
+
+    return (
+        <form onSubmit={props.loadWeather}>
+            <input type="text" name="city" placeholder="Which City ?" />
+            <input type="text" name="country" placeholder="What Country ?" />
+            <button>Search</button>
+        </form>
+    )
 }
 export default Form;
--- project-app/src/components/Weather.js
+++ project-app/src/components/Weather.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Weather = (props) => {
+    
     return(
         <div>  
             {props.country && props.city && <p>Location:{props.city},{props.country}</p>}
